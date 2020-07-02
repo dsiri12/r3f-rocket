0547a2a42bb573e0f5f0791b302240daf6e4d293
Damien Siri <dsiri12@gmail.com>
2020-07-02 18:02:21 -0400

    add cube with black color

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,32 @@
-import React from "react"
+import React, { useState } from "react"
+import { Canvas, applyProps } from 'react-three-fiber';
+import { useSpring, a } from 'react-spring/three';
 
-export default function Home() {
-  return <div>Hello world!</div>
+import './style.css'
+
+const Box = () => {
+  const [hovered, setHovered] = useState(false)
+  const [active, setActive] = useState(false)
+  const props = useSpring({
+    scale: active ? [1.5, 1.5, 1.5] : [1, 1, 1],
+    color: hovered ?  "hotpink" : "gray"
+  })
+
+  return (
+  <a.mesh 
+    onPointerOver={() => setHovered(true)} 
+    onPointerOut={() => setHovered(false)}
+    onClick={() => setActive(!active)}
+    scale={props.scale}
+  >
+      <boxBufferGeometry attach="geometry" args={[1, 1, 1]} />
+      <meshStandardMaterial attach="material" color={props.color} />
+    </a.mesh>
+  );
 }
+
+export default () => (
+  <Canvas>
+    <Box />
+  </Canvas>
+)
